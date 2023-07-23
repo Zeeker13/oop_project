e87775d74c3180237df629fe6703c42e8bf59ffe
Shohel Kashyapa <shohelkashapa809@gmail.com>
2023-07-23 19:43:41 +0530

    new file:   .idea/.gitignore 	new file:   .idea/compiler.xml 	new file:   .idea/encodings.xml 	new file:   .idea/jarRepositories.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/oop_project.iml 	new file:   .idea/uiDesigner.xml 	new file:   .idea/vcs.xml 	new file:   desktop.ini 	new file:   oop/oop_project-main/myshare1/src/main/java/example/view/inventory.form 	new file:   oop/oop_project-main/myshare1/src/main/java/example/view/inventory.java

---
+++ oop/oop_project-main/myshare1/src/main/java/example/view/inventory.java
@@ -0,0 +1,13 @@
+package example.view;
+
+import javax.swing.*;
+
+public class inventory {
+    private JPanel inventory;
+    private JTextField txt_in;
+    private JTextField txt_id;
+    private JTextField txt_price;
+    private JButton btn_add;
+    private JTextField txt_id2;
+    private JButton btn_remove;
+}
